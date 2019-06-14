0929d98fad08f5bbd3fe429a329833df2623a8b3
LuisTurcios <luisgustavoturcios62@gmail.com>
2019-06-14 10:32:03 -0600

    cursos a red etc

---
+++ src/app/Components/add-course-to-network/add-course-to-network.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-course-to-network',
+  templateUrl: './add-course-to-network.component.html',
+  styleUrls: ['./add-course-to-network.component.css']
+})
+export class AddCourseToNetworkComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/Components/list-course-to-network/list-course-to-network.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-course-to-network',
+  templateUrl: './list-course-to-network.component.html',
+  styleUrls: ['./list-course-to-network.component.css']
+})
+export class ListCourseToNetworkComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
