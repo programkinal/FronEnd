ac535bd550c9684626ef3f383120b5b36d93d732
Nebury <holasoyluis1998@gmail.com>
2019-06-13 19:13:57 -0600

    Hola

---
+++ src/app/models/enroll-students.ts
@@ -0,0 +1,9 @@
+export class EnrollStudents {
+    constructor(
+        public Student: String,
+        public unitAcademy: String,
+        public workingDay: String,
+        public grade: String,
+        public quota: String,
+    ){}
+}
